7721c79753ce71400433e021c0e5f0b0cd697b7e
Alex <eddings.sharanna@gmail.com>
2022-11-03 17:47:02 -0400

    updated version 1 file names to avoid confusion

---
+++ v1.js
@@ -0,0 +1,129 @@
+// fighter class only 6 for now
+
+// faction: Gang or Group affiliation
+// name: Character name
+// hp: hit points
+// body: defense points
+// attack: attack points
+// ability: Special Ability differs by faction
+
+// class Fighter {
+//   constructor(faction, name, hp, attack, body, ability) {
+//     this.faction = faction;
+//     this.name = name;
+//     this.hp = hp;
+//     this.attack = attack;
+//     this.body = body;
+//     this.ability = ability;
+//   }
+//   // methods
+//   regularAttack() {}
+//   specialAttack() {}
+// }
+
+// // Fighter Instances //
+// // each image will be attached to each fighter for player to select
+// // ! crop images for use
+
+// const fighter1 = new Fighter(
+//   "Rockerboy",
+//   "Johnny Silverhand",
+//   730, // adjust hp and attack amounts later for all fighters when functionality is working
+//   28,
+//   600,
+//   "Charisma"
+// );
+
+// // console.log(fighter1); - output works
+
+// const fighter2 = new Fighter(
+//   "Rockerboy",
+//   "Kerry Eurodyne",
+//   730,
+//   28,
+//   600,
+//   "Vocal Fry"
+// );
+
+// // console.log(fighter2); - output works
+
+// const fighter3 = new Fighter(
+//   "VooDoo Boys - Leader",
+//   "Maman Brigitte",
+//   730,
+//   25,
+//   600,
+//   "Interface"
+// );
+
+// // console.log(fighter3); - output works
+
+// const fighter4 = new Fighter(
+//   "MedTech - Ripperdoc",
+//   "Viktor Vektor",
+//   730,
+//   19,
+//   600,
+//   "Jury Rig"
+// );
+
+// // console.log(fighter4); - output works
+
+// const fighter5 = new Fighter(
+//   "Corpos",
+//   "Yorinobu Arasaka",
+//   730,
+//   23,
+//   600,
+//   "Cash Out"
+// );
+
+// // console.log(fighter5); - output works
+
+// const fighter6 = new Fighter(
+//   "Solo - Hired Assassin",
+//   "Adam Smasher",
+//   850,
+//   27,
+//   975,
+//   "Combat Sense"
+// );
+
+// // console.log(fighter6); - output works
+
+// // ! add more fighters if time permits once everything is working
+
+// const fighterOptionsArr = [];
+
+// for (let i = 0; i < 1; i++) {
+//   fighterOptionsArr.push(
+//     fighter1,
+//     fighter2,
+//     fighter3,
+//     fighter4,
+//     fighter5,
+//     fighter6
+//   );
+// }
+
+// // console.log(fighterOptionsArr); - output works
+
+// // Players //
+// const playerOne = [];
+// const playerTwo = [];
+
+// // const NightCity = {
+// //   round: 1,
+// //   playingGame: true,
+// //   playerOne: playerOne,
+// //   playerTwo: playerTwo,
+// //   runGame() {
+// //     this.fight();
+
+// //     this.gameOver();
+// //   },
+// //   gameOver() {
+// //     if (this.playingGame == false) {
+// //     }
+// //   },
+// // };
